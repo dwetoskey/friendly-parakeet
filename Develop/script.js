9151cc1c9b47f162dd2362aa70971f185e0c425e
dwetoskey <dwetoskey224@gmail.com>
2021-12-16 19:36:44 -0500

    added variables for password choices

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,10 @@
 // Assignment code here
-
-
-// Get references to the #generate element
+var capitalArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowercaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",  "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var numberArray = ["0", "1", "2", "3" , "4", "5", "6", "7", "8", "9"];
+var specialArray = ["!", "@", "#", "$", "%", "^", "&", "*", "-", "_", " ", "?", ".", "+"];
 var generateBtn = document.querySelector("#generate");
+
 
 // Write password to the #password input
 function writePassword() {
@@ -11,6 +13,10 @@ function writePassword() {
 
   passwordText.value = password;
 
+}
+
+function generatePassword() {
+  var pwlength = prompt;
 }
 
 // Add event listener to generate button
